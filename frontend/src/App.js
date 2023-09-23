8bb3c702657a97e6740ee2905c48135abe3778aa
Roya <Roya2002_rm@yahoo.com>
2023-09-23 13:48:40 +0300

    add creatContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from './routes'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+import AuthContext from "./Context/authContext";
 
 export default function App() {
-const router=useRoutes(routes)
-
-
+  const router = useRoutes(routes);
 
   return (
-    <div>{router}</div>
-  )
+    <AuthContext.Provider>
+  
+  {router}
+  </AuthContext.Provider>);
 }
