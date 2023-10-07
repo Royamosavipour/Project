0e7fa74bfebf44cb32a8984a3d7a0c38e0fb371f
Roya <Roya2002_rm@yahoo.com>
2023-10-07 10:58:25 +0300

    add swiper

--- frontend/src/Components/PreSellCourses/PreSellCourses.js
+++ frontend/src/Components/PreSellCourses/PreSellCourses.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeaders from "../SectionHeaders/SectionHeaders";
+import CourseBox from "../CourseBox/CourseBox";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 import "./PreSellCourses.css";
 
 export default function PreSellCourses() {
+  const [preselCourses, setPreselCourses] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/courses/presell`)
+      .then((res) => res.json())
+      .then((allpresellCourses) => {
+        console.log(allpresellCourses);
+        setPreselCourses(allpresellCourses);
+      });
+  }, []);
   return (
     <div>
       <div className="presell">
         <div className="container">
-          <SectionHeaders title={"دوره های در حال پیش فروش"} />
+          <SectionHeaders title={"دوره های در حال پیش فروش"} desc={"تست"} />
+
+          <div className="courses-content">
+            <div className="container">
+              <div className="row">
+                <Swiper
+                loop={true}
+                  slidesPerView={4}
+                  spaceBetween={30}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  className="mySwiper"
+                >
+                  {preselCourses.map((course) => (
+                    <SwiperSlide>
+                      <CourseBox {...course} isSlider={true} />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/Pages/Articels/Articels.js
+++ frontend/src/Pages/Articels/Articels.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import TopBar from "../../Components/TopBar/TopBar";
 import Footer from "../../Components/Footer/Footer";
 import BreadCrumb from "../../Components/BreadCrumb/BreadCrumb";
 import Pagination from "../../Components/Pagination/Pagination";
 import CourseBox from "../../Components/CourseBox/CourseBox";
-
-import "./Articels.css";
 import ArticleBox from "../../Components/ArticleBox/ArticleBox";
 
+import "./Articels.css";
+
 export default function Articels() {
+  const [articles, setArticles] = useState([]);
+  const [shownArticles, setShownArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/articles`)
+      .then((res) => res.json())
+      .then((allArticles) => {
+        console.log(allArticles);
+        setArticles(allArticles);
+      });
+  },[]);
   return (
     <>
       <TopBar />
-
       <Navbar />
       <BreadCrumb
         links={[
           { id: 1, title: "خانه", to: "" },
-          { id: 2, title:"تمامی مقالات", to: "/articles/1" },
+          { id: 2, title: "تمامی مقالات", to: "/articles/1" },
         ]}
       />
 
@@ -27,20 +37,20 @@ export default function Articels() {
           <div className="courses-content">
             <div className="container">
               <div className="row">
-              <ArticleBox 
-                
-              />
-                
+                {shownArticles.map(article=>(
+                <ArticleBox {...article} />
+
+                ))}
               </div>
             </div>
           </div>
 
-          {/* <Pagination
-            items={courses}
-            itemCount={5}
-            pathname={"/courses"}
-            setshowncourses={setShownCourses}
-          /> */}
+          <Pagination
+            items={articles}
+            itemCount={3}
+            pathname={"/articels"}
+            setshowncourses={setShownArticles}
+          />
         </div>
       </section>
 
