206047a9a7d5a89a4ed9979c17497a2ed4ef7f3d
Roya <Roya2002_rm@yahoo.com>
2023-10-07 11:24:53 +0300

    add popularCourses

--- frontend/src/Components/PopularCourses/PopularCourses.js
+++ frontend/src/Components/PopularCourses/PopularCourses.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeaders from "../SectionHeaders/SectionHeaders";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 import "./PopularCourses.css";
 
 export default function PopularCourses() {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/courses/popular`)
+      .then((res) => res.json())
+      .then((popularCourses) => {
+        console.log(popularCourses);
+      });
+  }, []);
+
   return (
     <>
       <div className="popular">
         <div className="container">
-          <SectionHeaders title={"محبوب ترین دوره ها"} />
+          <SectionHeaders title={"محبوب ترین دوره ها"} desc={"تست"} />
+
+          <div className="courses-content">
+            <div className="container">
+              <div className="row">
+                <Swiper
+                  slidesPerView={3}
+                  spaceBetween={30}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>Slide 1</SwiperSlide>
+                </Swiper>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
