ce1fe3eaeac4e55af18689064791c972d84913ef
Roya <Roya2002_rm@yahoo.com>
2023-10-18 21:11:12 +0300

    ok

--- frontend/src/Pages/AdminPanel/Users/Users.js
+++ frontend/src/Pages/AdminPanel/Users/Users.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const localStoragData = JSON.parse(localStorage.getItem("user"));
+    fetch(`http://localhost:4000/v1/users`, {
+      headers: { Authorization: `Bearer ${localStoragData.token}` },
+    })
+      .then((res) => res.json())
+      .then((allUsers) => {
+        console.log(allUsers);
+        setUsers(allUsers)
+      });
+  }, []);
   return (
     <>
       <DataTable title={"کاربران"}>
@@ -9,34 +21,36 @@ export default function Users() {
           <thead>
             <tr>
               <th>شناسه</th>
-              <th>نام</th>
-              <th>نام خانوادگی</th>
-              <th>شماره</th>
+              <th>نام خانوادگی نام</th>
               <th>ایمیل</th>
-              <th>رمز عبور</th>
               <th>ویرایش</th>
               <th>حذف</th>
+              <th>بن</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>34223</td>
-              <td>علی</td>
-              <td>سعیدی</td>
-              <td>09123443243</td>
-              <td>ali@gmail.com</td>
-              <td>ehsan1323</td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  ویرایش
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-danger delete-btn">
-                  حذف
-                </button>
-              </td>
-            </tr>
+            {users.map((user, index) => (
+              <tr>
+                <td>{index + 1} </td>
+                <td>{user.name} </td>
+                <td>{user.email} </td>
+                <td>
+                  <button type="button" class="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger delete-btn">
+                    حذف
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger delete-btn">
+                    بن
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </DataTable>
