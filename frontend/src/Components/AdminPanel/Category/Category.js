d55953b09de6c799ad949960dc771b0da1508982
Roya <Roya2002_rm@yahoo.com>
2023-10-23 23:05:54 +0300

    ok

--- frontend/src/Components/AdminPanel/Category/Category.js
+++ frontend/src/Components/AdminPanel/Category/Category.js
@@ -3,17 +3,58 @@ import DataTable from "../DataTable/DataTable";
 
 export default function Category() {
   const [categories, setCategories] = useState([]);
+  
   useEffect(() => {
+    getallCategories()
+  }, []);
+
+
+  function getallCategories(){
     fetch(`http://localhost:4000/v1/category`)
       .then((res) => res.json())
       .then((allCategories) => {
         console.log(allCategories);
+        setCategories(allCategories)
       });
-  }, []);
+  }
 
   return (
     <>
-      <DataTable title={"دسته بندی"}></DataTable>
+      <DataTable title={"دسته بندی"}>
+      <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+
+              <th>ویرایش</th>
+              <th>حذف</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((category, index) => (
+              <tr>
+                <td>{index + 1} </td>
+                <td>{category.title} </td>
+                <td>
+                  <button type="button" class="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-danger delete-btn"
+                  >
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DataTable>
     </>
   );
 }
