7f7909f2fad37c314f79ebb6208ab205284fc832
royamousavi <roya2002_rm@yahoo.com>
2023-10-25 19:38:55 +0300

    add input

--- frontend/src/Components/AdminPanel/Category/Category.js
+++ frontend/src/Components/AdminPanel/Category/Category.js
@@ -1,27 +1,130 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../DataTable/DataTable";
+import Input from "../../../Components/Form/Input";
+import { useForm } from "../../../Hooks/useForm";
+import swal from "sweetalert";
+
+import {
+  requiredValidator,
+  minValidator,
+  maxValidator,
+  emailValidator,
+} from "../../../Validaitors/rules";
+
+import "./Category.css";
 
 export default function Category() {
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      shortname: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
   const [categories, setCategories] = useState([]);
-  
+
   useEffect(() => {
-    getallCategories()
+    getallCategories();
   }, []);
 
-
-  function getallCategories(){
+  function getallCategories() {
     fetch(`http://localhost:4000/v1/category`)
       .then((res) => res.json())
       .then((allCategories) => {
         console.log(allCategories);
-        setCategories(allCategories)
+        setCategories(allCategories);
       });
   }
 
+  const createNewCategory = (event) => {
+    event.preventDefault();
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+    const newCategoryInfo = {
+      title: formState.inputs.title.value,
+      name: formState.inputs.shortname.value,
+    };
+
+    fetch("http://localhost:4000/v1/category", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+      body: JSON.stringify(newCategoryInfo),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        swal({
+          title: "دسته بندی مورد نظر با موفقیت اضافه شد",
+          icon: "success",
+          buttons: "اوکی",
+        }).then(() => {
+          getallCategories();
+        });
+      });
+  };
+
   return (
     <>
+      <div class="container-fluid" id="home-content">
+        <div class="container">
+          <div class="home-title">
+            <span>افزودن دسته‌بندی جدید</span>
+          </div>
+          <form class="form">
+            <div class="col-6">
+              <div class="name input">
+                <label class="input-title">عنوان</label>
+                <Input
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  type="text"
+                  id="title"
+                  placeholder="لطفا عنوان را وارد کنید..."
+                  validations={[minValidator(5), maxValidator(20)]}
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="name input">
+                <label class="input-title">اسم کوتاه</label>
+                <Input
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  type="text"
+                  id="shortname"
+                  placeholder="لطفا اسم کوتاه را وارد کنید..."
+                  validations={[minValidator(5), maxValidator(20)]}
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="bottom-form">
+                <div class="submit-btn">
+                  <input
+                    type="submit"
+                    value="افزودن"
+                    onClick={createNewCategory}
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <DataTable title={"دسته بندی"}>
-      <table class="table">
+        <table class="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -29,7 +132,6 @@ export default function Category() {
 
               <th>ویرایش</th>
               <th>حذف</th>
-
             </tr>
           </thead>
           <tbody>
@@ -43,10 +145,7 @@ export default function Category() {
                   </button>
                 </td>
                 <td>
-                  <button
-                    type="button"
-                    class="btn btn-danger delete-btn"
-                  >
+                  <button type="button" class="btn btn-danger delete-btn">
                     حذف
                   </button>
                 </td>
