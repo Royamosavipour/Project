60768a09708440c74175b4a0f9258db27426f144
royamousavi <roya2002_rm@yahoo.com>
2023-10-31 19:19:45 +0200

    ok

--- frontend/src/Pages/AdminPanel/AdminCourses/AdminCourses.js
+++ frontend/src/Pages/AdminPanel/AdminCourses/AdminCourses.js
@@ -1,20 +1,57 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
+import swal from "sweetalert";
 
 export default function AdminCourses() {
   const [allCourses, setAllCourses] = useState([]);
 
   useEffect(() => {
-    const localstorageData=JSON.parse(localStorage.getItem('user'))
-    fetch(`http://localhost:4000/v1/courses`,
-    {headers:{'Authorization':`Bearer ${localstorageData.token}`}})
+    getAllcourses();
+  }, []);
+
+  function getAllcourses() {
+    const localstorageData = JSON.parse(localStorage.getItem("user"));
+    fetch(`http://localhost:4000/v1/courses`, {
+      headers: { Authorization: `Bearer ${localstorageData.token}` },
+    })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setAllCourses(data)
-
+        setAllCourses(data);
       });
-  }, []);
+  }
+
+  const removeCourse = (coursID) => {
+    const localstorageData = JSON.parse(localStorage.getItem("user"));
+    swal({
+      title: "آیا از حذف مطمینی؟",
+      icon: "warning",
+      buttons: ["نه", "آره"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http//localhost:4000/v1/courses/${coursID}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${localstorageData.token}`,
+          },
+        }).then((result) => {
+          console.log(result);
+          if (result.ok) {
+            swal({
+              title: "حذف با موفقیت انجام شد",
+              buttons: "ok",
+              icon: "success",
+            }).then(() => getAllcourses());
+          } else {
+            swal({
+              title: "حذف با موفقیت انجام نشد",
+              buttons: "تلاش محدد",
+              icon: "error",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <>
@@ -38,8 +75,12 @@ export default function AdminCourses() {
               <tr>
                 <td>{index + 1} </td>
                 <td>{cours.name} </td>
-                <td>{cours.price===0?'رایگان':cours.price.toLocaleString()} </td>
-                <td>{cours.isComplete===1?'تکمیل شده ':'در حال برگزاری'} </td>
+                <td>
+                  {cours.price === 0 ? "رایگان" : cours.price.toLocaleString()}{" "}
+                </td>
+                <td>
+                  {cours.isComplete === 1 ? "تکمیل شده " : "در حال برگزاری"}{" "}
+                </td>
                 <td>{cours.shortName} </td>
                 <td>{cours.creator} </td>
                 <td>{cours.categoryID.title} </td>
@@ -49,7 +90,11 @@ export default function AdminCourses() {
                   </button>
                 </td>
                 <td>
-                  <button type="button" class="btn btn-danger delete-btn">
+                  <button
+                    type="button"
+                    class="btn btn-danger delete-btn"
+                    onClick={() => removeCourse(cours._id)}
+                  >
                     حذف
                   </button>
                 </td>
