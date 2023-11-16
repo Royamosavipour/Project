566fe4128a4a106b0316bcd1f11e57d34d8eb4a3
royamousavi <roya2002_rm@yahoo.com>
2023-11-16 18:04:11 +0200

    get all setioens

--- frontend/src/Pages/AdminPanel/Sessions/Sessions.js
+++ frontend/src/Pages/AdminPanel/Sessions/Sessions.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import Input from "../../../Components/Form/Input";
 import { useForm } from "../../../Hooks/useForm";
 import { minValidator } from "../../../Validaitors/rules";
+import swal from "sweetalert";
+import DataTable from './../../../Components/AdminPanel/DataTable/DataTable'
 
 export default function Sessions() {
   const [courses, setCourses] = useState([]);
   const [sessionCourse, setSessionCourse] = useState("-1");
   const [sessionVideo, setSessionVideo] = useState({});
+  const [sessionFree, setSessionFree] = useState("0");
+  const [setions, setSetions] = useState([]);
+
   const [formState, onInputHandler] = useForm(
     {
       title: {
@@ -22,6 +27,8 @@ export default function Sessions() {
   );
 
   useEffect(() => {
+    getAllSesstion();
+
     fetch(`http://localhost:4000/v1/courses`)
       .then((res) => res.json())
       .then((allcourses) => {
@@ -29,6 +36,39 @@ export default function Sessions() {
         setCourses(allcourses);
       });
   }, []);
+
+  const createSession = (e) => {
+    e.preventDefault();
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+    let formData = new FormData();
+    formData.append("title", formState.inputs.title.value);
+    formData.append("time", formState.inputs.time.value);
+    formData.append("video", sessionVideo);
+    formData.append("free", sessionFree);
+
+    fetch(`http://localhost:4000/v1/courses/${sessionCourse}/sessions`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${localStorageData.token}` },
+      body: formData,
+    }).then((res) => {
+      if (res.ok) {
+        swal({
+          title: "درخواست دوره ثیت گردید",
+          icon: "success",
+          buttons: "OK",
+        }).then(() => console.log("get all sesstion"));
+      }
+    });
+  };
+
+  function getAllSesstion() {
+    fetch(`http://localhost:4000/v1/courses/sessions`)
+      .then((res) => res.json())
+      .then((allsetions) => {
+        console.log(allsetions);
+        setSetions(allsetions);
+      });
+  }
 
   return (
     <>
@@ -55,7 +95,10 @@ export default function Sessions() {
             <div class="col-6">
               <div class="name input">
                 <label class="input-title">فایل جلسه</label>
-                <input type="file" onChange={e=>setSessionCourse(e.target.value)} />
+                <input
+                  type="file"
+                  onChange={(e) => setSessionCourse(e.target.value)}
+                />
                 <span class="error-message text-danger"></span>
               </div>
             </div>
@@ -96,7 +139,7 @@ export default function Sessions() {
             <div class="col-12">
               <div class="bottom-form">
                 <div class="submit-btn">
-                  <input type="submit" value="افزودن" />
+                  <input type="submit" value="افزودن" onClick={createSession} />
                 </div>
               </div>
             </div>
