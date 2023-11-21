69e285fe15353c89e75912599fa4fe573370ea42
royamousavi <roya2002_rm@yahoo.com>
2023-11-21 19:35:26 +0200

    ok

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopBar from "../TopBar/TopBar";
 import Navbar from "../Navbar/Navbar";
 import Landing from "../Landing/Landing";
 
 import "./Header.css";
+import { json } from "react-router-dom";
 
 export default function Header() {
+  const [indexInfo, setIndexInfo] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/infos/index`)
+      .then((res) => res.json())
+      .then((allInfo) => {
+        console.log(allInfo);
+      });
+  }, []);
   return (
     <header className="header">
       <TopBar />
